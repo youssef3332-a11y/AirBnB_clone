09de272b2b46a3ca9b8886d56e0a2800e3684d87
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-27 13:56:46 +0100

    new file

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""BaseModel class for Airbnb project."""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModel class for Airbnb project."""
+    def __init__(self):
+        """constractor"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        """__str__ override"""
+        name = self.__class__.__name__
+        result = "[{}] ({}) {}".format(name, self.id, self.__dict__)
+        return result
+
+    def save(self):
+        """ update date"""
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """to dictionary"""
+        dit = self.__dict__.copy()
+        dit["__class__"] = self.__class__.__name__
+        dit['created_at'] = self.created_at.isoformat()
+        dit['updated_at'] = self.updated_at.isoformat()
+        return dit
