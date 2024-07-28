1966ebe3214c8adfa5d2363fc10d47e84cdb10e7
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-28 17:18:17 +0100

    new file

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for HBNB"""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the programn\n"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line input"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
