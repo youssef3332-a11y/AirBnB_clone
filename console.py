4aeb3e1539dc90cb3349c4c8006771eb9c0fff9f
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-29 12:16:32 +0100

    new

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import cmd
-
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for HBNB"""
@@ -13,13 +14,114 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, arg):
         """EOF command to exit the program"""
-        print()
         return True
 
     def emptyline(self):
         """Do nothing on empty line input"""
         pass
 
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it and prints the id"""
+        if not arg:
+            print("** class name missing **")
+            return
+        try:
+            cls = globals()[arg]
+            obj = cls()
+            obj.save()
+            print(obj.id)
+        except KeyError:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in globals():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        print(storage.all()[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in globals():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        del storage.all()[key]
+        storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances based or not on the class name"""
+        if arg and arg not in globals():
+            print("** class doesn't exist **")
+            return
+        objects = storage.all()
+        result = []
+        for key, obj in objects.items():
+            if not arg or key.startswith(arg):
+                result.append(str(obj))
+        print(result)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding or updating attribute"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        if args[0] not in globals():
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = f"{args[0]}.{args[1]}"
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        obj = storage.all()[key]
+        attr_name = args[2]
+        attr_value = args[3].strip('\"')
+        
+        # Cast attribute value to the correct type
+        if hasattr(obj, attr_name):
+            attr_type = type(getattr(obj, attr_name))
+            try:
+                casted_value = attr_type(attr_value)
+            except ValueError:
+                casted_value = attr_value
+        else:
+            casted_value = attr_value
+        
+        setattr(obj, attr_name, casted_value)
+        obj.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
