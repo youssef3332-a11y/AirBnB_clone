70c7b2374ce8c802eec301fb53b38a46b4314705
youssef3332-a11y <youssef17102003@gmail.com>
2024-07-30 16:14:24 +0100

    new file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,12 @@ import json
 import os
 from models.base_model import BaseModel
 from models.user import User
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenty import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
